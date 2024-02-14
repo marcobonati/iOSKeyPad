b538aa306f6a6e6094fe7c4f0849f56f9564411e
Marco Bonati <marco.bonati@fabrick.com>
2024-02-14 15:17:34 +0100

    minor

--- iOSKeypad/Views/Keypad/KeypadView.swift
+++ iOSKeypad/Views/Keypad/KeypadView.swift
@@ -8,19 +8,17 @@
 import SwiftUI
 
 struct KeypadView: View, KeypadButtonDelegate {
-    
     @Binding var showSecondaryButtons: Bool
     @Binding var values: [Double]
 
     @State private var internalBuffer: String = ""
-//    private var model: KeypadModel = .normalKeypad()
-//    private var secondaryModel: KeypadModel = .mathOperationsKeypad()
     private let textConverter = KeypadViewTextConverter(decimals: 2)
     
     init(values: Binding<[Double]>,
-         showSecondaryButtons: Binding<Bool>) {
-           _values = values
-           _showSecondaryButtons = showSecondaryButtons
+         showSecondaryButtons: Binding<Bool>)
+    {
+        _values = values
+        _showSecondaryButtons = showSecondaryButtons
     }
     
     public var body: some View {
@@ -34,13 +32,13 @@ struct KeypadView: View, KeypadButtonDelegate {
                                                    delegate: self,
                                                    text: "2"))
                     KeypadButton(KeypadButtonModel(buttonType: .Numeric_3,
-                                                          delegate: self,
-                                                          text: "3"))
+                                                   delegate: self,
+                                                   text: "3"))
                     if showSecondaryButtons {
                         KeypadButton(KeypadButtonModel(buttonType: .Operator_Divide,
-                                                            delegate: self,
-                                                             text: "",
-                                                             image: Image(systemName: "divide"))
+                                                       delegate: self,
+                                                       text: "",
+                                                       image: Image(systemName: "divide"))
                         )
                     }
                 }
@@ -56,9 +54,9 @@ struct KeypadView: View, KeypadButtonDelegate {
                                                    text: "6"))
                     if showSecondaryButtons {
                         KeypadButton(KeypadButtonModel(buttonType: .Operator_Multiply,
-                                                            delegate: self,
-                                                             text: "",
-                                                             image: Image(systemName: "multiply"))
+                                                       delegate: self,
+                                                       text: "",
+                                                       image: Image(systemName: "multiply"))
                         )
                     }
                 }
@@ -74,9 +72,9 @@ struct KeypadView: View, KeypadButtonDelegate {
                                                    text: "9"))
                     if showSecondaryButtons {
                         KeypadButton(KeypadButtonModel(buttonType: .Operator_Minus,
-                                                            delegate: self,
-                                                             text: "",
-                                                             image: Image(systemName: "minus"))
+                                                       delegate: self,
+                                                       text: "",
+                                                       image: Image(systemName: "minus"))
                         )
                     }
                 }
@@ -93,26 +91,24 @@ struct KeypadView: View, KeypadButtonDelegate {
                                                    image: Image(systemName: "delete.left.fill")))
                     if showSecondaryButtons {
                         KeypadButton(KeypadButtonModel(buttonType: .Operator_Plus,
-                                                            delegate: self,
-                                                             text: "",
-                                                             image: Image(systemName: "plus"))
+                                                       delegate: self,
+                                                       text: "",
+                                                       image: Image(systemName: "plus"))
                         )
                     }
                 }
-                
             }
-        }.onAppear {
         }
     }
     
     func onButtonLongPress(button: KeypadButtonType) {
         if button == .Accessory_Delete {
-            self.internalBuffer = ""
-            self.values[values.count-1] = 0
+            internalBuffer = ""
+            values[values.count-1] = 0
         }
         
         if button == .Numeric_00 {
-            withAnimation(.snappy(duration:0.3)){
+            withAnimation(.snappy(duration: 0.3)) {
                 self.showSecondaryButtons.toggle()
             }
         }
@@ -130,7 +126,7 @@ struct KeypadView: View, KeypadButtonDelegate {
         }
     }
     
-    private func handleNumericPressed(_ button: KeypadButtonType){
+    private func handleNumericPressed(_ button: KeypadButtonType) {
         if (button == .Numeric_0 || button == .Numeric_00) && internalBuffer.isEmpty {
             return
         }
@@ -138,147 +134,44 @@ struct KeypadView: View, KeypadButtonDelegate {
         valueDidChanged(internalBuffer)
     }
 
-    private func handleDeletePressed(_ button: KeypadButtonType){
+    private func handleDeletePressed(_ button: KeypadButtonType) {
         if !internalBuffer.isEmpty {
             internalBuffer.removeLast()
         }
         valueDidChanged(internalBuffer)
     }
 
-    private func handleOperatorPressed(_ button: KeypadButtonType){
+    private func handleOperatorPressed(_ button: KeypadButtonType) {
         commitCurrentValue()
     }
     
-    private func valueDidChanged(_ stringValue: String){
-        if (self.values.isEmpty){
-            self.values.append(0)
+    private func valueDidChanged(_ stringValue: String) {
+        if values.isEmpty {
+            values.append(0)
         }
-        self.values[values.count-1] = textConverter.keypadTextToDouble(stringValue) ?? 0
+        values[values.count-1] = textConverter.keypadTextToDouble(stringValue) ?? 0
     }
 
-    private func commitCurrentValue(){
-        self.values.append(0)
-        self.internalBuffer = ""
+    private func commitCurrentValue() {
+        values.append(0)
+        internalBuffer = ""
     }
-    
 }
-//
-//class KeypadButtonDelegateProxy: KeypadButtonDelegate {
-//    var delegate: KeypadButtonDelegate?
-//    func onButtonLongPress(button: KeypadButtonType) {
-//        delegate?.onButtonLongPress(button: button)
-//    }
-//    func onButtonPressed(button: KeypadButtonType) {
-//        delegate?.onButtonPressed(button: button)
-//    }
-//}
-//
-//class KeypadModel: ObservableObject, KeypadButtonDelegate {
-//    var buttons: [KeypadButtonModel]
-//    var buttonsDelegateProxy: KeypadButtonDelegateProxy
-//    var delegate: KeypadButtonDelegate?
-//    var textValue: String
-//    
-//    init(buttons: [KeypadButtonModel], buttonsDelegate: KeypadButtonDelegateProxy) {
-//        self.textValue = ""
-//        self.buttons = buttons
-//        self.buttonsDelegateProxy = buttonsDelegate
-//        self.buttonsDelegateProxy.delegate = self
-//    }
-//
-//    func onButtonPressed(button: KeypadButtonType) {
-//        //_onButtonPressed(button: button)
-//        delegate?.onButtonPressed(button: button)
-//    }
-//    
-//    func onButtonLongPress(button: KeypadButtonType) {
-//        delegate?.onButtonLongPress(button: button)
-//    }
-//
-//}
-//
-//extension KeypadModel {
-//    static func mathOperationsKeypad() -> KeypadModel {
-//        let delegateProxy = KeypadButtonDelegateProxy()
-//        var buttons: [KeypadButtonModel] = []
-//        buttons.append(KeypadButtonModel(buttonType: .Operator_Divide,
-//                                         delegate: delegateProxy,
-//                                         text: "",
-//                                         image: Image(systemName: "divide")))
-//        buttons.append(KeypadButtonModel(buttonType: .Operator_Multiply,
-//                                         delegate: delegateProxy,
-//                                         text: "",
-//                                         image: Image(systemName: "multiply")))
-//        buttons.append(KeypadButtonModel(buttonType: .Operator_Minus,
-//                                         delegate: delegateProxy, 
-//                                         text: "",
-//                                         image: Image(systemName: "minus")))
-//        buttons.append(KeypadButtonModel(buttonType: .Operator_Plus,
-//                                         delegate: delegateProxy, 
-//                                         text: "",
-//                                         image: Image(systemName: "plus")))
-//        return KeypadModel(buttons: buttons, buttonsDelegate: delegateProxy)
-//    }
-//
-//    static func normalKeypad() -> KeypadModel {
-//        let delegateProxy = KeypadButtonDelegateProxy()
-//        var buttons: [KeypadButtonModel] = []
-//        buttons.append(KeypadButtonModel(buttonType: .Numeric_1,
-//                                         delegate: delegateProxy,
-//                                         text: "1"))
-//        buttons.append(KeypadButtonModel(buttonType: .Numeric_2,
-//                                         delegate: delegateProxy,
-//                                         text: "2"))
-//        buttons.append(KeypadButtonModel(buttonType: .Numeric_3,
-//                                         delegate: delegateProxy,
-//                                         text: "3"))
-//        buttons.append(KeypadButtonModel(buttonType: .Numeric_4,
-//                                         delegate: delegateProxy,
-//                                         text: "4"))
-//        buttons.append(KeypadButtonModel(buttonType: .Numeric_5,
-//                                         delegate: delegateProxy,
-//                                         text: "5"))
-//        buttons.append(KeypadButtonModel(buttonType: .Numeric_6,
-//                                         delegate: delegateProxy,
-//                                         text: "6"))
-//        buttons.append(KeypadButtonModel(buttonType: .Numeric_7,
-//                                         delegate: delegateProxy,
-//                                         text: "7"))
-//        buttons.append(KeypadButtonModel(buttonType: .Numeric_8,
-//                                         delegate: delegateProxy,
-//                                         text: "8"))
-//        buttons.append(KeypadButtonModel(buttonType: .Numeric_9,
-//                                         delegate: delegateProxy,
-//                                         text: "9"))
-//        buttons.append(KeypadButtonModel(buttonType: .Numeric_00,
-//                                         delegate: delegateProxy,
-//                                         text: "00"))
-//        buttons.append(KeypadButtonModel(buttonType: .Numeric_0,
-//                                         delegate: delegateProxy,
-//                                         text: "0"))
-//        buttons.append(KeypadButtonModel(buttonType: .Accessory_Delete,
-//                                         delegate: delegateProxy,
-//                                         text: "", 
-//                                         image: Image(systemName: "delete.left.fill")))
-//        return KeypadModel(buttons: buttons, buttonsDelegate: delegateProxy)
-//    }
-//}
 
-fileprivate struct KeypadViewTextConverter {
-    
+private struct KeypadViewTextConverter {
     var decimals: Int
     let numberFormatter = NumberFormatter()
     let locale = Locale.current
     var decimalSeparator: String
     
-    init(decimals: Int){
+    init(decimals: Int) {
         self.decimals = decimals
-        self.numberFormatter.numberStyle = .decimal
-        self.numberFormatter.locale = locale
+        numberFormatter.numberStyle = .decimal
+        numberFormatter.locale = locale
         self.decimalSeparator = locale.decimalSeparator ?? "."
     }
     
-    func keypadTextToDouble(_ input: String)-> Double?  {
+    func keypadTextToDouble(_ input: String) -> Double? {
         let stringNumber = makeStringWithDecimal(padStringWithZeros(input))
         if let formattedNumber = numberFormatter.number(from: stringNumber) {
             return formattedNumber.doubleValue
@@ -287,7 +180,7 @@ fileprivate struct KeypadViewTextConverter {
         }
     }
     
-    func makeStringWithDecimal(_ input: String)-> String {
+    func makeStringWithDecimal(_ input: String) -> String {
         guard input.count >= decimals else {
             return input
         }
@@ -304,10 +197,9 @@ fileprivate struct KeypadViewTextConverter {
             return input
         }
     }
-    
 }
 
-//struct ContentView_Previews: PreviewProvider {
+// struct ContentView_Previews: PreviewProvider {
 //    static var previews: some View {
 //        VStack {
 //            Spacer()
@@ -321,4 +213,4 @@ fileprivate struct KeypadViewTextConverter {
 //            Spacer()
 //        }
 //    }
-//}
+// }
